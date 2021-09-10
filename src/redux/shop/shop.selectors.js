00b5cfb988197bf9c33eeaf799daf07f5dec99a2
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-09-10 20:10:50 +0300

    add currying function to get collection array in collection component with advance selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,17 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.routeName === collectionUrlParam
+    )
+  )
 );
