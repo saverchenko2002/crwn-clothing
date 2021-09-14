22a0214a4827b0bc16cfa49da8d513747716f5fc
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-09-14 11:31:15 +0300

    change observable to promise, add fetch for external api

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -25,13 +25,17 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/crwn-db/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json)
+    //   .then((collections) => console.log(collections));
   }
 
   render() {
